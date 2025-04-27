c88d60338cf1723b0080e56ea5ff8c8970fd2ff9
Naflan <naflan265@gmail.com>
2025-04-27 15:19:05 +0530

    updates

---
+++ lib/App.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Flutter Object Detection",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 20.0,
+            ),
+          ),
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.cyan.shade600, Colors.blue.shade800],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+          ),
+          centerTitle: true,
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.white10, Colors.blue.shade300], // Lighter shades for the body
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text("my app", style: TextStyle(fontSize: 24)),
+              ),
+              // Add more widgets here
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
